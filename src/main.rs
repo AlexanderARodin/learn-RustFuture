cf5ef465fa7be9239c3091e5f759e00655ee8417
AlexanderARodin <rodin_aa@inbox.ru>
2023-05-12 02:51:33 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+#![allow(non_snake_case)]
 
-
+use futures::executor::block_on;
+use futures::task::Poll::Ready;
 
 
 fn main() {
-    println!("started..");
+    println!("\nstarted..");
     
-    let a = ;
+    let futu = generateString();
+    block_on(futu);
+    
+    match futu {
+        Pending => println!("<--> pending"),
+        Ready(()) => println!("<--> ready: {}", "<>"),
+    }
+
+    let a = "<>";
     println!("test output: {}", a);
 
     println!("..ended");
 }
 
 
-fn generateString() -> String {
-    "gen str"
+async fn generateString() { //-> String {
+    println!("[inside]");
+    //"gen str".to_string()
 }
